4ca70d38363872246556f72126e1b5f257dda8af
meetg21 <94597802+meetg21@users.noreply.github.com>
2022-05-11 16:34:09 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,6 @@
+# creating a band name for the user 
+
+print("Welcome to the band Name GEnerator")
+x = input("What's name of the city you grew up in?\n")
+y = input("What's your pet's name?\n")
+print("Your band name could be " + x + " " + y )
